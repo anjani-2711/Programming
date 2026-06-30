2d2da619e2c6d3ee7cf8113e8a998601455b1a43
anjani-2711 <atambe.2711@gmail.com>
2026-06-30 10:43:37 +0530

    Add files via upload

---
+++ C_Programming/Competitive/Assignments/Program24.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int Factorial(int iNo)
+{
+    int iCnt = 0;
+    int iFact = 1;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = iNo; iCnt >= 1; iCnt--)
+    {
+        iFact = iFact * iCnt;
+    }
+
+    return iFact;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+
+    iRet = Factorial(iValue);
+
+    printf("Factorial of number is %d\n",iRet);
+
+    return 0;
+}
